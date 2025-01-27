886f9774b4e4806fda2ee964e25f51acfae3c55a
saikrishna123-wo <pavankumargottipati1994@gmail.com>
2025-01-27 11:44:26 +0530

    Added Node.js app and Dockerfile

---
+++ my-node-app/app.js
@@ -0,0 +1,11 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node.js running in Docker!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+
